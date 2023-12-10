7652e6d47ab22db7a197b89ce0cabcce6ec1c1ab
santyTT <santiprocastellar7@gmail.com>
2023-12-10 18:26:12 -0500

    nuevo

---
+++ Trabajo De Eduardo Rios/BACKEND/formularo.php
@@ -0,0 +1,10 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$messaje = $_POST['messaje'];
+  
+  echo "<h2>informacion</h2>";
+  echo "<h2>el nombre es</h2" . $name <br>"";
+  echo "<h2>el email es</h2>" . $email <br>"";
+  echo "<h2>el messaje es</h2>" . $messaje <br>";
+?>
