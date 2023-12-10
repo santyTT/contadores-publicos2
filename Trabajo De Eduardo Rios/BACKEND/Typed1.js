f470622519e40793a00374f7bc6f08e755cba5cc
santyTT <santiprocastellar7@gmail.com>
2023-12-10 18:23:08 -0500

    hola

--- Trabajo De Eduardo Rios/BACKEND/Typed1.js
+++
@@ -1,8 +0,0 @@
-const typed = new Typed('.typed', {
-    String: [
-             '<i>hola</i>',
-             '<i>buenas</i>',
-             '<i>muchachos</i>'
-            ]
-});
-console.log("holaaa");
